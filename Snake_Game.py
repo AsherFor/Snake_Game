d5a42a82bb4db5caf2bf35f7b5dfaedde546431a
AsherForman <asherforman@hs.postoakschool.org>
2020-11-19 10:54:40 -0600

    Commit

--- Snake_Game.py
+++ Snake_Game.py
@@ -5,7 +5,9 @@ Up = False
 Down = False
 Left = False
 Right = False
-keep_playing = True
+stop_or_keep_playing = True
+apple = turtle.Turtle()
+
 
 #Draw Screen
 window = turtle.Screen()
@@ -23,64 +25,37 @@ snake_head.penup()
 snake_head.goto(0, 0)
 
 # Make Apple
-apple = turtle.Turtle()
-apple.shape("circle")
-apple.color("#ff0000")
-apple.penup()
-apple.goto(0, 0)
-ran_x = random.randrange(-250, 250, 10)
-ran_y = random.randrange(-250, 250, 10)
-apple.setpos(ran_x, ran_y)
+def spawn_apple():
+    global apple
+    apple.shape("circle")
+    apple.color("#ff0000")
+    apple.penup()
+    apple.goto(0, 0)
+    ran_x = random.randrange(-250, 250, 10)
+    ran_y = random.randrange(-250, 250, 10)
+    apple.setpos(ran_x, ran_y)
 
 
 
-def check_boundaries():
-    global keep_playing
+def check_boundaries(apple):
+    global stop_or_keep_playing
     if snake_head.ycor() <= -245 or snake_head.ycor() >= 245:
-        keep_playing = False
+        stop_or_keep_playing = False
         close_text()
-    if snake_head.xcor() <= -250 or snake_head.xcor() >= 245:
+    if snake_head.xcor() <= -245 or snake_head.xcor() >= 245:
+        stop_or_keep_playing = False
         close_text()
-    if ((apple.ycor() <= snake_head.ycor() + 5 and apple.ycor() >= snake_head.ycor() - 5) and
-            (apple.xcor() <= snake_head.xcor() + 5 and apple.xcor() >= snake_head.xcor() - 5)):
-            apple.setpos(ran_x, ran_y)
+    if ((apple.ycor() <= snake_head.ycor() + 12 and apple.ycor() >= snake_head.ycor() - 12) and
+            (apple.xcor() <= snake_head.xcor() + 12 and apple.xcor() >= snake_head.xcor() - 12)):
+            spawn_apple()
+
+
 
 
 def close_text():
     close = turtle.Turtle()
     close.hideturtle()
     close.write("Wall hit", align="center", font=("Courier", 24, "normal"))
-
-# def up_move():
-#     y = snake_head.ycor()
-#     y += 20
-#     snake_head.sety(y)
-#     print(x, y)
-#     boundaries()
-#
-#
-# def down_move():
-#     y = snake_head.ycor()
-#     y -= 20
-#     snake_head.sety(y)
-#     print(x ,y)
-#     boundaries()
-#
-#
-# def left_move():
-#     x = snake_head.xcor()
-#     x -= 20
-#     snake_head.setx(x)
-#     print(x, y)
-#     boundaries()
-#
-#
-# def right_move():
-#     x = snake_head.xcor()
-#     x += 20
-#     snake_head.setx(x)
-#     print(x, y)
-#     boundaries()
 
 def head_up():
     global Up, Down, Left, Right
@@ -114,7 +89,8 @@ def close_game():
     exit()
 
 def game_play():
-    while keep_playing is True:
+    global Up, Down, Left, Right
+    while stop_or_keep_playing == True:
         if Up == True:
             y = snake_head.ycor()
             y += 5
@@ -134,8 +110,8 @@ def game_play():
             x = snake_head.xcor()
             x += 5
             snake_head.setx(x)
+        check_boundaries(apple)
         window.update()
-        check_boundaries()
 
 
 # Keyboard bindings
@@ -146,6 +122,7 @@ window.onkeypress(head_left, "a")
 window.onkeypress(head_right, "d")
 window.onkeypress(close_game, "Escape")
 
+spawn_apple()
 game_play()
 turtle.done()
 
